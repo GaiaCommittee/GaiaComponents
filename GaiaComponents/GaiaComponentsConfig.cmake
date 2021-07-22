b7cccaab5008e15db1fc3a29740f9177bd69f1f0
Vincent <rumble.vecent@outlook.com>
2021-07-22 20:48:56 +0800

    Change install script, add version support.

--- GaiaComponents/GaiaComponentsConfig.cmake
+++
@@ -1,8 +0,0 @@
-cmake_minimum_required(3.10)
-
-find_path(GaiaComponents_INCLUDE_DIRS "GaiaComponents")
-find_library(GaiaComponents_LIBS "GaiaComponents")
-
-if (GaiaComponents_INCLUDE_DIRS AND GaiaComponents_LIBS)
-    set(GaiaComponents_FOUND TRUE)
-endif()
